b217d2742e280e920b71b2095c76cb0a7cd22e41
Maximo Bautista <max.bautista97@gmail.com>
2019-01-09 16:42:42 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each do |key, value|
+    
+    value.each do |language, info|
+      
+     if new_hash[language][:style] 
+       new_hash[language][:style] << key
+     else
+       new_hash[language] = info.merge!({:style => [key]})
+     end
+      
+    end
+    
+  end
+ 
+    binding.pry 
+
 end
